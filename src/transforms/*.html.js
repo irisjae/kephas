cd18f1cca1199a8910e544c01aca809f4b225807
mumenrider <brian.aquinde@outlook.com>
2017-10-22 10:14:21 +0000

    failed attempt at tagging
    attempted using widgets
    turns out widgets mess with layouting

--- src/transforms/*.html.js
+++ src/transforms/*.html.js
@@ -1,29 +1,9 @@
 var R = require ('ramda');
 
 module .exports = function (str) {
-    return str .replace (/(class=")([^"]+)(")/g, function (match, before, classes, after) {
-        var class_list = classes .split (' ');
-        var partition = R .partition (function (s) {
-            return s .indexOf ('as-') !== -1
-        }) (class_list);
-        var new_attributes = '';
-        if (partition [0] .length) {
-            partition [0] .forEach (function (class_) {
-                var slice = class_ .split ('as-');
-                if (slice [0])
-                    new_attributes += (' ' + 'id="' + slice [0] + '"');
-                slice .slice (1) .forEach (function (attr) {
-                    var partition = attr .split ('-of-');
-                    if (partition .length === 1)
-                        new_attributes += (' ' + attr);
-                    else {
-                        var attr_name = partition [0];
-                        var attr_value = partition .slice (1) .join ('-of-');
-                        new_attributes += (' ' + attr_name + '-"' + attr_value + '"');
-                    }
-                })
-            });
-        }
-        return before + class_list .join (' ') + after + new_attributes;
+    return str .replace (/(<[^/>]+)(>)<div class=(?:meta|"widget\d+")>((?!<\/div>).*)<\/div>/g, function (match, open, opening, meta) {
+        return open + ' ' + meta + opening;
+    }) .replace (/<div class=(?:meta|"widget\d+")>((?!<\/div>).*)<\/div>(<[^/>]+)(>)/g, function (match, meta, open, opening) {
+        return open + ' ' + meta + opening;
     });
 };
